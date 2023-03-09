e20b84038e0d0dbdd20e1093f83a687bd92ab9ea
Amr Mohamed <mero@Amrs-MacBook-Air.local>
2023-03-08 23:02:12 -0330

    creating the final capstone for the IOS

---
+++ LittleLemonCapstone/LittleLemonCapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonCapstoneApp.swift
+//  LittleLemonCapstone
+//
+//  Created by Amr Mohamed on 2023-03-08.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonCapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
